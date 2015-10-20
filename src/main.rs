0aedee6f0e364b6aff30b8b4d8a3d9bbb26605c4
chrkolst <christopher.kolstad@finn.no>
2015-10-20 09:10:45 +0200

    Added body and query string parsing to simple server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate iron;
+extern crate bodyparser;
+extern crate regex;
+extern crate persistent;
+extern crate urlencoded;
+
+use regex::Regex;
+use persistent::Read;
+use iron::status;
+use iron::prelude::*;
+use urlencoded::UrlEncodedQuery;
+
+const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
+fn extremequestion(req: &mut Request) -> IronResult<Response> {
+    let body = req.get::<bodyparser::Raw>();
+    match body {
+        Ok(Some(body)) => println!("Read body:\n{}", body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err)
+    };
+    match req.get_ref::<UrlEncodedQuery>() {
+        Ok(ref hashmap) => println!("Parsed GET Request query string:\n {:?}", hashmap),
+        Err(ref e) => println!("{:?}", e)
+    };
+    Ok(Response::with(status::Ok))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut chain = Chain::new(extremequestion);
+    chain.link_before(Read::<bodyparser::MaxBodyLength>::one(MAX_BODY_LENGTH));
+    Iron::new(chain).http("localhost:4999").unwrap();
+    println!("Running at port 4999");
 }
